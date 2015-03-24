22257d5833c5139c310b4c126117f46ff952e071
Alexey Kulikov <a.kulikov@gmail.com>
2015-03-24 17:03:55 +0100

    Database Structure Started

--- resources/db/db.sql
+++ resources/db/db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE companies(
-  company_id INT UNSIGNED NOT NULL,
+  company_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   created DATETIME NOT NULL,
   modified TIMESTAMP,
@@ -9,7 +9,7 @@ CREATE TABLE companies(
 
 
 CREATE TABLE users(
-  user_id INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   company_id INT UNSIGNED NOT NULL,
@@ -20,22 +20,38 @@ CREATE TABLE users(
   FOREIGN KEY (company_id) REFERENCES companies(company_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) Engine=InnoDB;
 
+CREATE TABLE grow_status(
+  status_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+
+  created DATETIME NOT NULL,
+  modified TIMESTAMP,
+
+  PRIMARY KEY (status_id),
+  KEY(name)
+) Engine=InnoDB;
+
+INSERT INTO grow_status(name, created) VALUES('planning', NOW()),('planted',NOW()),('vegetation',NOW()),('flowering',NOW()),('checking',NOW()),('done',NOW()),('canceled',NOW());
 
 CREATE TABLE grow(
-  grow_id INT UNSIGNED NOT NULL,
+  grow_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   company_id INT UNSIGNED NOT NULL,
   user_id INT UNSIGNED NOT NULL,
+  status_id INT UNSIGNED DEFAULT NULL,
+
   created DATETIME NOT NULL,
   modified TIMESTAMP,
 
   PRIMARY KEY (grow_id),
+  KEY(status),
   FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (company_id) REFERENCES companies(company_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (company_id) REFERENCES companies(company_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (status_id) REFERENCES grow_status(status_id) ON DELETE SET NULL ON UPDATE CASCADE
 ) Engine=InnoDB;
 
 
 CREATE TABLE plant_types(
-  type_id INT UNSIGNED NOT NULL,
+  type_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   created DATETIME NOT NULL,
   modified TIMESTAMP,
@@ -43,16 +59,24 @@ CREATE TABLE plant_types(
 ) ENGINE=InnoDB;
 
 CREATE TABLE plant(
-  plant_id INT UNSIGNED NOT NULL,
+  plant_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   grow_id INT UNSIGNED NOT NULL,
 
   name VARCHAR(100) NOT NULL,
   type_id INT UNSIGNED DEFAULT NULL,
+
+  status_id INT UNSIGNED DEFAULT NULL,
+
+  planted_on DATETIME DEFAULT NULL,
+  vegetation_on DATETIME DEFAULT NULL,
+  flowering_on DATETIME DEFAULT NULL,
+  chop_chop_on DATETIME DEFAULT NULL,
 
   created DATETIME NOT NULL,
   modified TIMESTAMP,
 
   PRIMARY KEY (plant_id),
   FOREIGN KEY (grow_id) REFERENCES grow(grow_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (type_id) REFERENCES plant_types(type_id) ON DELETE SET NULL ON UPDATE CASCADE
+  FOREIGN KEY (type_id) REFERENCES plant_types(type_id) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (status_id) REFERENCES grow_status(status_id) ON DELETE SET NULL ON UPDATE CASCADE
 ) Engine=InnoDB;
